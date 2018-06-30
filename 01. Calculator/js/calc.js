fa1c501d5c3a350cd7de4571e6b1c1b2e4be2f7b
blackdev7 <kristiyan.stefanov@mail.com>
2018-06-30 22:26:22 +0300

    - Bug fixes

--- 01. Calculator/js/calc.js
+++ 01. Calculator/js/calc.js
@@ -1,9 +1,7 @@
 function calculate(){
-
     const regex = /(\d+\.?\d{0,7})/g;
     const invalidInput = /([^\d+\.?\d|*\/+-])/;
-    const operatorEnd = /[*/+-]$/;
-
+    const operatorEnd = /[*/+-\.]$/;
     var input = document.getElementById('equation').value;
 
     if(input.match(invalidInput) || input.match(operatorEnd)){
@@ -13,28 +11,35 @@ function calculate(){
     else{
 
         if(input.length > 254){
-
             alert("Maximum character lenght: 255, try with less characters!");
-
-        } else{
+        } else {
             var result = eval(input);
-            if(result == "Infinity"){
+            
+            if(result == "Infinity") {
                 alert("Cannot divide by 0");
-            }
-            else{
+            } else {
                 alert("Result: " + result);
             }
         }
     }
-
 }
 
 function removeLast() {
     var currentEq = document.getElementById('equation').value;
     document.getElementById('equation').value = currentEq.substring(0, currentEq.length-1);
+
 }
 
-function addToEquation(charInput){
-
-    document.getElementById('equation').value += charInput;
+function addToEquation(charInput) {
+    const operators = ["-", "+", "*", "/", "."];
+    var currentEQ = document.getElementById('equation').value;
+    var lastCharOfEQ = currentEQ[currentEQ.length-1];
+    if(operators.includes(charInput)) {
+       if(operators.includes(lastCharOfEQ)){
+        document.getElementById('equation').value = currentEQ.substring(0, currentEQ.length-1);
+       }
+       document.getElementById('equation').value += charInput;
+    } else {
+        document.getElementById('equation').value += charInput;
+    }
 }
