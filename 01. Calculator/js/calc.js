efc2f19afa2764a3546ada71c90d70d1ba5c5475
blackdev7 <kristiyan.stefanov@mail.com>
2018-06-30 19:13:20 +0300

    - Updating to new calc 1.0

--- 01. Calculator/js/calc.js
+++ 01. Calculator/js/calc.js
@@ -1,52 +1,28 @@
 function calculate(){
-    var firstNumber = document.getElementById('firstNumber').value;
-    var secondNumber = document.getElementById('secondNumber').value;
-    var operator = document.getElementById("operator");
-    var operatorVal = operator.options[operator.selectedIndex].value;
 
-    const regex = /^-?\d+\.?\d{0,10}$/gm;
+    const regex = /(\d+\.?\d{0,10})/g;
+    const invalidInput = /([^\d+\.?\d{0,10}|*\/+-])/;
+    const operatorEnd = /[*/+-]$/;
+    const operators = /[*/+-]/;
 
-        if(firstNumber.match(regex) && secondNumber.match(regex))
-        {
-            firstNumber = parseFloat(firstNumber);
-            secondNumber = parseFloat(secondNumber);
-            var result = 0;
-            switch (operatorVal)
-            {
-                case "+":
-                    result = firstNumber + secondNumber;
-                    break;
-                case "-":
-                    result = firstNumber - secondNumber;
-                    break;
-                case "*":
-                    result = firstNumber * secondNumber;
-                    break;
-                case "/":
-                    if (firstNumber == 0 || secondNumber == 0)
-                    {
-                        alert("Cannot divide by 0");
-                        return;
-                    }
-                    else
-                    {
-                        result = firstNumber / secondNumber;
-                    }
-                    break;
-                case "%":
-                    result = firstNumber % secondNumber;
-                    break;
-            }
-            alert("Result: " + result);
+    var input = document.getElementById('equation').value;
+
+    if(input.match(invalidInput) || input.match(operatorEnd)){
+        alert("Wrong Input");
+    }
+    else{
+
+        var matches = regex.exec(input);
+        console.dir(matches.length);
+        for(var i=0; i<matches.length; i++){
+            console.dir(matches[i].value);
         }
-        else
-        {
-            alert("Invalid input, please try again!")
-        }
+
+    }
+
 }
 
-function clearCalc()
-{
-   document.getElementById('firstNumber').value = '';
-   document.getElementById('secondNumber').value = '';
+function removeLast() {
+    var currentEq = document.getElementById('equation').value;
+    document.getElementById('equation').value = currentEq.substring(0, currentEq.length-1);
 }
