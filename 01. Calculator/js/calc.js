a69407ea7f122423c12261ea1ce3fbac1ef9a382
blackdev7 <kristiyan.stefanov@mail.com>
2018-06-30 20:46:09 +0300

    - Fixed a couple of bugs

--- 01. Calculator/js/calc.js
+++ 01. Calculator/js/calc.js
@@ -1,23 +1,30 @@
 function calculate(){
 
-    const regex = /(\d+\.?\d{0,10})/g;
-    const invalidInput = /([^\d+\.?\d{0,10}|*\/+-])/;
+    const regex = /(\d{0,10}\.?\d{0,10})/g;
+    const invalidInput = /([^\d+\.?\d|*\/+-])/;
     const operatorEnd = /[*/+-]$/;
-    const operators = /[*/+-]/;
 
     var input = document.getElementById('equation').value;
 
     if(input.match(invalidInput) || input.match(operatorEnd)){
         alert("Wrong Input");
     }
+
     else{
 
-        var matches = regex.exec(input);
-        console.dir(matches.length);
-        for(var i=0; i<matches.length; i++){
-            console.dir(matches[i].value);
-        }
+        if(input.length > 254){
 
+            alert("Maximum character lenght: 255, try with less characters!");
+
+        } else{
+            var result = eval(input);
+            if(result == "Infinity"){
+                alert("Cannot divide by 0");
+            }
+            else{
+                alert("Result: " + result);
+            }
+        }
     }
 
 }
